4dbc85c4df41a1826196564f89f25e5af08db8fb
guirlviana <guirlviana@gmail.com>
2024-02-13 21:54:26 -0300

    feat: usecase for create user

---
+++ core/usecases/investor_usecase.py
@@ -0,0 +1,15 @@
+from db.models import Investor
+from django.contrib.auth.models import User
+
+def create_user(name, last_name, phone, email, password):
+    user = User(email=email, username=email)
+    user_already_exists = User.objects.filter(email=email, username=email).exists()
+    if user_already_exists:
+        raise Exception
+    
+    user.set_password(password)
+    user.save()
+
+    investor = Investor(name=name, last_name=last_name, phone=phone, user=user)
+    investor.save()
+    return investor
