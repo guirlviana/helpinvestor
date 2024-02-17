d01e100596b794d39f2f591639640a85445ec241
guirlviana <guirlviana@gmail.com>
2024-02-17 18:43:06 -0300

    update: new fields for asset

--- core/serializers/asset_serializer.py
+++ core/serializers/asset_serializer.py
@@ -3,3 +3,5 @@ from rest_framework import serializers
 class AssetSerializer(serializers.Serializer):
     symbol = serializers.CharField(max_length=8, required=True)
     wallet_id = serializers.IntegerField(min_value=1, required=True)
+    buy_price = serializers.DecimalField(min_value=1, decimal_places=2, max_digits=5)
+    sale_price = serializers.DecimalField(min_value=1, decimal_places=2, max_digits=5)
