f41cf59bf64256236ecdc18038d265a1d84974bd
guirlviana <guirlviana@gmail.com>
2024-02-21 21:43:55 -0300

    update: add a todo and early return if user already exists

--- core/usecases/investor_usecase.py
+++ core/usecases/investor_usecase.py
@@ -2,10 +2,11 @@ from db.models import Investor, Wallet
 from django.contrib.auth.models import User
 
 def create_investor(name, last_name, phone, email, password):
-    user = User(email=email, username=email)
     user_already_exists = User.objects.filter(email=email, username=email).exists()
     if user_already_exists:
-        raise Exception
+        raise Exception('Usuario existente') # todo: response with bad request
+    
+    user = User(email=email, username=email)
     
     user.set_password(password)
     user.save()
