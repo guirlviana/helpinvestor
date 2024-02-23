50dee3da37565638dbd65956b54909eb94c9d613
guirlviana <guirlviana@gmail.com>
2024-02-22 22:52:18 -0300

    update: add env variables for service to get assets price

--- configs/settings.py
+++ configs/settings.py
@@ -26,6 +26,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
+ASSETS_API_KEY = os.environ.get('ASSETS_API_KEY')
+ASSETS_API_URL = 'https://www.alphavantage.co'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if os.environ.get('DEBUG') == '1' else False
 
