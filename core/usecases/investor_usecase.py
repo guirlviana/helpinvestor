8b475ed49bbc0f9db8e89e8bb4932d93cdeedd83
guirlviana <guirlviana@gmail.com>
2024-03-01 22:23:42 -0300

    update: translate exception

--- core/usecases/investor_usecase.py
+++ core/usecases/investor_usecase.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 def create_investor(name, last_name, phone, email, password):
     user_already_exists = User.objects.filter(email=email, username=email).exists()
     if user_already_exists:
-        raise Exception('Usuario existente') # todo: response with bad request
+        raise Exception('User is invalid') # todo: response with bad request
     
     user = User(email=email, username=email)
     
