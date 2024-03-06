9d32a68ec8f0db9d7df67b8c20254365eb65bd69
guirlviana <guirlviana@gmail.com>
2024-03-05 22:53:36 -0300

    update: readable code

--- services/lambdas_aws.py
+++ services/lambdas_aws.py
@@ -3,25 +3,37 @@ import requests
 import os
 
 def task_send_quotes_in_sms():
-    response = requests.post(f'{settings.APP_URL}/api-token-auth/', {
-        "username": os.environ.get('ADMIN_EMAIL'),
-        "password": os.environ.get('ADMIN_PASSWORD')
-    })
+    token = __authenticate_as_admin()
+    if not token:
+        return
 
-    if response.status_code != 200:
-        return response
-    
-    response = response.json()
-    token = response['token']
-
-    headers = {'Authorization': f'Token {token}'}
-    response = requests.get(f'{settings.APP_URL}/get-share-prices/', headers=headers)
-    notifications = response['response']
+    notifications = __get_quote_prices_notifications()
     if not notifications:
         return
     
     for notification in notifications:
         __mock_send_sms(notification['phone'], notification['message'])
+
+
+def __authenticate_as_admin():
+    response = requests.post(f'{settings.APP_URL}/api-token-auth/', {
+        "username": os.environ.get('ADMIN_EMAIL'),
+        "password": os.environ.get('ADMIN_PASSWORD')
+    })
+    
+    if response.status_code != 200:
+        return None
+    
+    response = response.json()
+    return response['token']
+
+
+def __get_quote_prices_notifications():
+    headers = {'Authorization': f'Token {token}'}
+    response = requests.get(f'{settings.APP_URL}/get-share-prices/', headers=headers)
+    if response.status_code != 200:
+        return None
+    return response['response']
 
 
 def __mock_send_sms(phone, message):
