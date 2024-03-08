220fca2af484268b625e4f16665d758bf023ad2e
guirlviana <guirlviana@gmail.com>
2024-03-07 23:06:48 -0300

    update: dont allow create phone with spaces or special chars

--- core/serializers/investor_serializer.py
+++ core/serializers/investor_serializer.py
@@ -6,3 +6,13 @@ class InvestorSerializer(serializers.Serializer):
     email = serializers.EmailField(required=True, allow_blank=False)
     password = serializers.CharField(required=True, allow_blank=False, max_length=30)
     phone = serializers.CharField(required=True, allow_blank=False, max_length=30)
+
+    def validate_phone(self, value):
+        if not value:
+            raise serializers.ValidationError("Phone cannot be empty")
+
+        cleaned_value = value.lstrip('+')
+        if not cleaned_value.isdigit():
+            raise serializers.ValidationError("Phone must contain only numeric characters and may have a leading '+'")
+
+        return value
