b2115e08d7feb6d30ae37af107e6528cb9eab047
guirlviana <guirlviana@gmail.com>
2024-03-11 19:29:10 -0300

    update: separate assets urls

--- configs/urls.py
+++ configs/urls.py
@@ -19,13 +19,16 @@ from django.urls import path
 from api import investor_api, asset_api
 from rest_framework.authtoken import views
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api-token-auth/', views.obtain_auth_token),
-    path('investor/', investor_api.create_user),
+assets_urls = [
     path('create-asset/', asset_api.create_asset),
     path('get-assets/', asset_api.get_assets),
     path('edit-asset/<int:id>/', asset_api.edit_asset),
     path('delete-asset/<int:id>/', asset_api.delete_asset),
-    path('get-share-prices/', asset_api.get_share_prices),
 ]
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api-token-auth/', views.obtain_auth_token),
+    path('investor/', investor_api.create_user),
+    path('get-share-prices/', asset_api.get_share_prices),
+] + assets_urls
