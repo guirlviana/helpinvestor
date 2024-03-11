79585326e3095f614b46a5bd72439cde7d93b6e9
guirlviana <guirlviana@gmail.com>
2024-03-11 19:07:15 -0300

    update: new param for select fields to validate

--- core/serializers/asset_serializer.py
+++ core/serializers/asset_serializer.py
@@ -5,3 +5,12 @@ class AssetSerializer(serializers.Serializer):
     wallet_id = serializers.IntegerField(min_value=1, required=True)
     buy_price = serializers.DecimalField(min_value=1, decimal_places=2, max_digits=5)
     sale_price = serializers.DecimalField(min_value=1, decimal_places=2, max_digits=5)
+
+    def __init__(self, *args, fields_to_validate=None, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        if fields_to_validate:
+            allowed = set(fields_to_validate)
+            existing = set(self.fields.keys())
+            for field in existing - allowed:
+                self.fields.pop(field)
