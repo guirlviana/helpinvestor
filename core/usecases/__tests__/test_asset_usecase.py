4b8b314f091181f38a764c54ec8274fd327276d3
guirlviana <guirlviana@gmail.com>
2024-03-12 20:21:35 -0300

    feat: add test

--- core/usecases/__tests__/test_asset_usecase.py
+++ core/usecases/__tests__/test_asset_usecase.py
@@ -7,3 +7,18 @@ class CreateAssetTests(TestCase):
     def setUp(self) -> None:
         self.investor = create_investor('john', 'doe', '11999999999', 'email@email.com', 'abcdfg123')
         self.wallet_id = self.investor.wallet_set.get().id
+    
+    def test_should_create_asset(self):
+        data = {
+            'wallet_id': self.wallet_id,
+            'symbol': 'TAEE4', 
+            'buy_price': 10, 
+            'sale_price': 15,
+        }
+
+        new_asset = create_asset(**data)
+
+        asset = Asset.objects.get(id=new_asset.id)
+        self.assertEqual(data['symbol'], asset.symbol)
+        self.assertEqual(data['buy_price'], asset.buy_price)
+        self.assertEqual(data['sale_price'], asset.sale_price)
