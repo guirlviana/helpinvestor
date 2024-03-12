6ae32e9339933970c5747833d4bbb7dfe7d1834a
guirlviana <guirlviana@gmail.com>
2024-03-12 20:30:20 -0300

    feat: create a testCase for assets

--- core/usecases/__tests__/test_asset_usecase.py
+++ core/usecases/__tests__/test_asset_usecase.py
@@ -3,6 +3,11 @@ from core.usecases.asset_usecase import create_asset
 from core.usecases.investor_usecase import create_investor
 from db.models import Asset
 
+class AssetsTestCase(TestCase):
+    def setUp(self) -> None:
+        self.investor = create_investor('john', 'doe', '11999999999', 'email@email.com', 'abcdfg123')
+        self.wallet_id = self.investor.wallet_set.get().id
+
 class CreateAssetTests(TestCase):
     def setUp(self) -> None:
         self.investor = create_investor('john', 'doe', '11999999999', 'email@email.com', 'abcdfg123')
