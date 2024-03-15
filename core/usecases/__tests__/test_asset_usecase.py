d479e9c4770d63d9637600957f37d98558c5706a
guirlviana <guirlviana@gmail.com>
2024-03-14 21:39:27 -0300

    update: reformat file

--- core/usecases/__tests__/test_asset_usecase.py
+++ core/usecases/__tests__/test_asset_usecase.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from core.usecases.asset_usecase import create_asset, delete_asset, edit_asset, get_assets
+from core.usecases.asset_usecase import create_asset, delete_asset, edit_asset, get_assets, get_assets_on_target_prices
 from core.usecases.investor_usecase import create_investor
 from db.models import Asset
 
@@ -14,12 +14,12 @@ class CreateAssetTests(AssetsTestCase):
     def setUp(self) -> None:
         self.investor = create_investor('john', 'doe', '11999999999', 'email@email.com', 'abcdfg123')
         self.wallet_id = self.investor.wallet_set.get().id
-    
+
     def test_should_create_asset(self):
         data = {
             'wallet_id': self.wallet_id,
-            'symbol': 'TAEE4', 
-            'buy_price': 10, 
+            'symbol': 'TAEE4',
+            'buy_price': 10,
             'sale_price': 15,
         }
 
@@ -29,32 +29,32 @@ class CreateAssetTests(AssetsTestCase):
         self.assertEqual(data['symbol'], asset.symbol)
         self.assertEqual(data['buy_price'], asset.buy_price)
         self.assertEqual(data['sale_price'], asset.sale_price)
-    
+
     def test_error_when_symbol_not_in_available_symbols(self):
         data = {
             'wallet_id': self.wallet_id,
-            'symbol': 'HPIN3', 
-            'buy_price': 10, 
+            'symbol': 'HPIN3',
+            'buy_price': 10,
             'sale_price': 15,
         }
 
         with self.assertRaises(Exception) as e:
             create_asset(**data)
-        
+
         self.assertEqual(f"Symbol: {data['symbol']} not valid, options: ITSA4, TAEE4, BBSE3", str(e.exception))
 
     def test_error_when_try_create_symbol_again(self):
         data = {
             'wallet_id': self.wallet_id,
-            'symbol': 'TAEE4', 
-            'buy_price': 10, 
+            'symbol': 'TAEE4',
+            'buy_price': 10,
             'sale_price': 15,
         }
 
         create_asset(**data)
         with self.assertRaises(Exception) as e:
             create_asset(**data)
-        
+
         self.assertEqual(f"Symbol: {data['symbol']} already created", str(e.exception))
 
 
@@ -62,8 +62,8 @@ class GetAssetsTests(AssetsTestCase):
     def test_should_return_all_assets(self):
         data = {
             'wallet_id': self.wallet_id,
-            'symbol': 'TAEE4', 
-            'buy_price': 10, 
+            'symbol': 'TAEE4',
+            'buy_price': 10,
             'sale_price': 15,
         }
         create_asset(**data)
@@ -77,8 +77,8 @@ class GetAssetsTests(AssetsTestCase):
     def test_should_return_only_fields_available(self):
         data = {
             'wallet_id': self.wallet_id,
-            'symbol': 'TAEE4', 
-            'buy_price': 10, 
+            'symbol': 'TAEE4',
+            'buy_price': 10,
             'sale_price': 15,
         }
         create_asset(**data)
@@ -100,29 +100,29 @@ class EditAssetTests(AssetsTestCase):
         self.assertEqual('TAEE4', asset_edited.symbol)
         self.assertEqual(asset.buy_price, asset_edited.buy_price)
         self.assertEqual(asset.sale_price, asset_edited.sale_price)
-    
+
     def test_error_when_try_update_not_allowed_fields(self):
         asset = self.__create_asset(symbol='BBSE3')
-        
+
         with self.assertRaises(Exception) as e:
             edit_asset(asset.id, self.wallet_id, new_values={'id': 99})
-        
+
         self.assertEqual('Fields to update not allowed', str(e.exception))
-    
+
     def test_error_when_try_update_assets_that_doesnt_exists(self):
         invalid_id = 1
         with self.assertRaises(Exception) as e:
             edit_asset(invalid_id, self.wallet_id, new_values={})
-        
+
         self.assertEqual('Asset does not exists', str(e.exception))
 
     def __create_asset(self, **kwargs):
         default = {
-            'wallet_id': self.wallet_id,
-            'symbol': 'TAEE4', 
-            'buy_price': 10, 
-            'sale_price': 15,
-        } | kwargs
+                      'wallet_id': self.wallet_id,
+                      'symbol': 'TAEE4',
+                      'buy_price': 10,
+                      'sale_price': 15,
+                  } | kwargs
 
         return create_asset(**default)
 
@@ -131,20 +131,20 @@ class DeleteAssetTests(AssetsTestCase):
     def test_should_delete_field(self):
         asset = create_asset(
             wallet_id=self.wallet_id,
-            symbol='TAEE4', 
-            buy_price=10, 
+            symbol='TAEE4',
+            buy_price=10,
             sale_price=15,
         )
-        
+
         delete_asset(asset.id, self.wallet_id)
 
         self.assertFalse(Asset.objects.filter(id=asset.id).exists())
-    
+
     def test_error_when_delete_asset_that_doesnt_exists(self):
         invalid_id = 99
         with self.assertRaises(Exception) as e:
             delete_asset(invalid_id, self.wallet_id)
-        
+
         self.assertEqual('Asset does not exists', str(e.exception))
 
 
